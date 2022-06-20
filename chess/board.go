4db3159e20f8dbef75715126a3768a87071e58e2
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-20 09:29:00 -0700

    Add more color methods

--- chess/board.go
+++ chess/board.go
@@ -12,3 +12,18 @@ const (
 func (c Color) Opposite() Color {
 	return c ^ 1
 }
+
+// Valid returns true if the color is valid.
+func (c Color) Valid() bool {
+	return c <= Black
+}
+
+// FEN returns the FEN representation of the color.
+func (c Color) FEN() string {
+	return []string{"w", "b"}[c]
+}
+
+// String returns the string representation of the color.
+func (c Color) String() string {
+	return []string{"White", "Black"}[c]
+}
