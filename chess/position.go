388ddba144b3d67513e425603bd119c297c1cba7
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-21 14:02:50 -0700

    Change NewPosition to pre-populate starting pieces

--- chess/position.go
+++ chess/position.go
@@ -11,8 +11,9 @@ type Position struct {
 	FullMoves  uint16
 }
 
+// NewPosition returns a new position, pre-populated with all starting pieces.
 func NewPosition() *Position {
-	return &Position{
+	p := Position{
 		Board:      [12]Bitboard{},
 		Castling:   NewCastlingRights(),
 		EnPassant:  NoEnPassant,
@@ -20,8 +21,31 @@ func NewPosition() *Position {
 		HalfMoves:  0,
 		FullMoves:  0,
 	}
+	p.Put(WhiteRook, A1)
+	p.Put(WhiteKnight, B1)
+	p.Put(WhiteBishop, C1)
+	p.Put(WhiteQueen, D1)
+	p.Put(WhiteKing, E1)
+	p.Put(WhiteBishop, F1)
+	p.Put(WhiteKnight, G1)
+	p.Put(WhiteRook, H1)
+	for sq := A2; sq <= H2; sq++ {
+		p.Put(WhitePawn, sq)
+	}
+	for sq := A7; sq <= H7; sq++ {
+		p.Put(BlackPawn, sq)
+	}
+	p.Put(BlackRook, A8)
+	p.Put(BlackKnight, B8)
+	p.Put(BlackBishop, C8)
+	p.Put(BlackQueen, D8)
+	p.Put(BlackKing, E8)
+	p.Put(BlackBishop, F8)
+	p.Put(BlackKnight, G8)
+	p.Put(BlackRook, H8)
+	return &p
 }
 
-func (p *Position) Put(piece Piece, square Square) {
-	p.Board[piece] |= Bitboard(1) << square
+func (p *Position) Put(pc Piece, s Square) {
+	p.Board[pc].Set(s)
 }
