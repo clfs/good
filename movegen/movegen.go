98b37dbb77bf5d20fbf8ace2635f9fde0a7b6186
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-25 11:54:59 -0700

    Add movegen package

---
+++ movegen/movegen.go
@@ -0,0 +1,15 @@
+package movegen
+
+import "github.com/clfs/good/chess"
+
+func IsLegalMove(p chess.Position, m chess.Move) bool {
+	return false
+}
+
+func PseudoLegalMoves(p chess.Position) []chess.Move {
+	return nil
+}
+
+func LegalMoves(p chess.Position) []chess.Move {
+	return nil
+}
---
+++ movegen/table.go
@@ -0,0 +1,106 @@
+package movegen
+
+import "github.com/clfs/good/chess"
+
+// Table is a collection of move lookup tables.
+var Table [12]map[chess.Square][]chess.Move
+
+// Lookup returns a slice of all possible moves for the given piece and square.
+// Moves are not guaranteed to be legal or in any particular order.
+func Lookup(p chess.Piece, s chess.Square) []chess.Move {
+	return Table[p][s]
+}
+
+func init() {
+	// Instantiation.
+	Table = [12]map[chess.Square][]chess.Move{}
+	for i := 0; i < 12; i++ {
+		Table[i] = make(map[chess.Square][]chess.Move)
+	}
+
+	// [DONE] White pawns moving one square up without promoting.
+	for s := chess.A2; s <= chess.H6; s++ {
+		move := chess.NewMove(s, s.Up())
+		Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+	}
+
+	// [DONE] White pawns moving two squares up.
+	for s := chess.A2; s <= chess.H2; s++ {
+		move := chess.NewMove(s, s.UpN(2))
+		Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+	}
+
+	// [DONE] White pawns capturing without promoting.
+	for s := chess.A2; s <= chess.H6; s++ {
+		if !s.IsLeftEdge() {
+			move := chess.NewMove(s, s.Up().Left())
+			Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+		}
+		if !s.IsRightEdge() {
+			move := chess.NewMove(s, s.Up().Right())
+			Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+		}
+	}
+
+	// [DONE] White pawns promoting without capturing.
+	for s := chess.A7; s <= chess.H7; s++ {
+		for pc := chess.WhiteKnight; pc <= chess.WhiteQueen; pc++ {
+			move := chess.NewPromotionMove(s, s.Up(), pc)
+			Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+		}
+	}
+
+	// [DONE] White pawns both capturing and promoting.
+	for s := chess.A7; s <= chess.H7; s++ {
+		for pc := chess.WhiteKnight; pc <= chess.WhiteQueen; pc++ {
+			if !s.IsLeftEdge() {
+				move := chess.NewPromotionMove(s, s.Up().Left(), pc)
+				Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+			}
+			if !s.IsRightEdge() {
+				move := chess.NewPromotionMove(s, s.Up().Right(), pc)
+				Table[chess.WhitePawn][s] = append(Table[chess.WhitePawn][s], move)
+			}
+		}
+	}
+
+	// [DONE] Black pawns moving one square down without promoting.
+	for s := chess.A3; s <= chess.H7; s++ {
+		move := chess.NewMove(s, s.Down())
+		Table[chess.BlackPawn][s] = append(Table[chess.BlackPawn][s], move)
+	}
+
+	// [DONE] Black pawns moving two squares down.
+	for s := chess.A7; s <= chess.H7; s++ {
+		move := chess.NewMove(s, s.DownN(2))
+		Table[chess.BlackPawn][s] = append(Table[chess.BlackPawn][s], move)
+	}
+
+	// Black pawns capturing without promoting.
+
+	// Black pawns promoting without capturing.
+
+	// Black pawns both capturing and promoting.
+
+	// White knight moves.
+
+	// White bishop moves.
+
+	// White rook moves.
+
+	// White queen moves.
+
+	// White king moves, not including castling.
+
+	// White castling moves.
+
+	// [DONE] Black knight, bishop, rook, and queen moves.
+	Table[chess.BlackKnight] = Table[chess.WhiteKnight]
+	Table[chess.BlackBishop] = Table[chess.WhiteBishop]
+	Table[chess.BlackRook] = Table[chess.WhiteRook]
+	Table[chess.BlackQueen] = Table[chess.WhiteQueen]
+
+	// Black king moves, not including castling.
+
+	// Black castling moves.
+}
