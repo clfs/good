bde9fad8457f11c2a88cad3c304093cc530999f9
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-26 12:53:39 -0700

    Remove old references to refgen package

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -3,15 +3,14 @@ package movegen
 
 import (
 	"github.com/clfs/good/chess"
-	"github.com/clfs/good/movegen/internal/refgen"
 )
 
 // IsLegalMove returns true if a move is legal in a position.
 func IsLegalMove(p chess.Position, m chess.Move) bool {
-	return refgen.IsLegalMove(p, m)
+	return false
 }
 
 // LegalMoves returns a slice of all legal moves in a position.
 func LegalMoves(p chess.Position) []chess.Move {
-	return refgen.LegalMoves(p)
+	return nil
 }
