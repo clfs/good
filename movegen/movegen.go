d45262e4ecd427091734023bfaf9af1b17ad5a50
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-26 07:35:59 -0700

    Remove chess.movegen.PseudoLegalMoves

--- movegen/movegen.go
+++ movegen/movegen.go
@@ -6,10 +6,6 @@ func IsLegalMove(p chess.Position, m chess.Move) bool {
 	return false
 }
 
-func PseudoLegalMoves(p chess.Position) []chess.Move {
-	return nil
-}
-
 func LegalMoves(p chess.Position) []chess.Move {
 	return nil
 }
