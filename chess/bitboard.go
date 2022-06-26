a82956651ad3a1a3b40a4f793fe30dd2c5411ec8
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-25 21:25:33 -0700

    Add more Bitboard methods

--- chess/bitboard.go
+++ chess/bitboard.go
@@ -1,5 +1,7 @@
 package chess
 
+import "strings"
+
 // Bitboard is an integer where each bit represents one square.
 // From LSB to MSB, the bits represent a1, b1, ..., h1, a2, ..., h8.
 type Bitboard uint64
@@ -24,6 +26,16 @@ func (b *Bitboard) Toggle(s Square) {
 	*b ^= s.Bitboard()
 }
 
+// IsEmpty returns true if the bitboard has no bits set.
+func (b *Bitboard) IsEmpty() bool {
+	return *b == 0
+}
+
+// Intersects returns true if two bitboards have any bits in common.
+func (b *Bitboard) Intersects(other *Bitboard) bool {
+	return *b&*other != 0
+}
+
 // SetIf sets the bit at a square to 1 if cond is true.
 func (b *Bitboard) SetIf(s Square, cond bool) {
 	if cond {
@@ -31,4 +43,20 @@ func (b *Bitboard) SetIf(s Square, cond bool) {
 	} else {
 		b.Clear(s)
 	}
+}
+
+// DebugString returns a multi-line string representation of the bitboard.
+func (b *Bitboard) DebugString() string {
+	var s strings.Builder
+	for r := Rank8; r <= Rank8; r-- {
+		for f := FileA; f <= FileH; f++ {
+			if b.Get(NewSquare(f, r)) {
+				s.WriteByte('1')
+			} else {
+				s.WriteByte('.')
+			}
+		}
+		s.WriteByte('\n')
+	}
+	return s.String()
 }
