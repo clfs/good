94370d2ff2d2c171512096dfa691d74d3c9aa91c
Calvin Figuereo-Supraner <mail@calvin.page>
2022-06-25 21:12:32 -0700

    Improve chess.Bitboard docstrings

--- chess/bitboard.go
+++ chess/bitboard.go
@@ -1,32 +1,32 @@
 package chess
 
 // Bitboard is an integer where each bit represents one square.
-// From LSB to MSB, the bits represent a1, b1, ..., h8.
+// From LSB to MSB, the bits represent a1, b1, ..., h1, a2, ..., h8.
 type Bitboard uint64
 
-// Set sets the given square to 1.
+// Set sets the bit at a square to 1.
 func (b *Bitboard) Set(s Square) {
 	*b |= s.Bitboard()
 }
 
-// Clear sets the given square to 0.
+// Clear clears the bit at a square to 0.
 func (b *Bitboard) Clear(s Square) {
 	*b &^= s.Bitboard()
 }
 
-// Get returns true if the given square is set.
+// Get returns true if the bit at a square is 1.
 func (b *Bitboard) Get(s Square) bool {
 	return *b&s.Bitboard()>>s != 0
 }
 
-// Toggle toggles the bit at the given square.
+// Toggle toggles the bit at a square.
 func (b *Bitboard) Toggle(s Square) {
 	*b ^= s.Bitboard()
 }
 
-// Assign assigns the provided value at a given square.
-func (b *Bitboard) Assign(s Square, v bool) {
-	if v {
+// SetIf sets the bit at a square to 1 if cond is true.
+func (b *Bitboard) SetIf(s Square, cond bool) {
+	if cond {
 		b.Set(s)
 	} else {
 		b.Clear(s)
